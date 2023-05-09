51730b3376e8922d27a7fcc13b2db7ed749fb6c6
Mr. Wolf <mr.wolf@pop-os.localdomain>
2023-05-09 21:03:33 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.9-slim-buster
+
+WORKDIR /app
+
+COPY . /app
+
+RUN apt-get update -y && \
+    pip install --trusted-host pypi.python.org -r requirements.txt
+
+EXPOSE 5000
+
+ENV FLASK_APP=app.py
+
+CMD ["flask", "run", "--host", "0.0.0.0"]
---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        return f'Hello, {first_name} {last_name}!<br>({email})<br>'
+    else:
+        return '''
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>SQlink Simple Flask App</title>
+                    <link rel="stylesheet" href="{{ url_for('static', filename='css/apple-style.css') }}">
+            </head>
+            <body>
+                <form method="POST">
+                    <label>First name:</label><br>
+                    <input type="text" name="first_name"><br>
+                    <label>Last name:</label><br>
+                    <input type="text" name="last_name"><br>
+                    <label>Email:</label><br>
+                    <input type="email" name="email"><br><br>
+                    <input type="submit" value="Submit">
+                </form>
+            </body>
+            </html>
+        '''
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
